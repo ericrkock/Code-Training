5dfa7f2a10e8483cda5466aa7be1f611b0286e89
Eric R. Kock <eric.r.kock@gmail.com>
2019-01-28 12:32:08 +0000

    Update Quote Machine - DT

--- FreeCodeCamp/Projects/Front End Libraries/Random Quote Machine/quote-machine/src/App.js
+++ FreeCodeCamp/Projects/Front End Libraries/Random Quote Machine/quote-machine/src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faIgloo } from '@fortawesome/free-solid-svg-icons'
+
+// library.add(faIgloo)
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="body">
+        <header className="quote-header">
+          <h1>Random Quote Machine</h1>
         </header>
+        <div className="quote-box">
+          <div className="quote-text" style={{opacity: 1}}>
+            <i className="fa fa-quote-left"></i>
+            <span id="quote-text">
+              Quote comes here
+            </span>
+          </div>
+          <div className="quote-author">
+            <span id="author">
+              - Author's name comes here
+            </span>
+          </div>
+          <div className="quote-buttons">
+            <a id="tweet-quote" className="tweet-button" title="Tweet this code" target="_blank" href="#"><i className="fa fa-twitter"></i></a>
+            <a id="tumblr-quote" className="tumblr-button" title="Post this quote on Tumblr" target="_blank" href="#"><i className="fa fa-tumblr"></i></a>
+            <button id="new-quote" className="new-quote-button">New Quote</button>
+          </div>
+
+        </div>
+        <footer>
+                    <div className="quote-box-footer">
+            Presented by <a href="#" target="_blank">Eric R. Kock</a>
+          </div>
+        </footer>
       </div>
     );
   }
