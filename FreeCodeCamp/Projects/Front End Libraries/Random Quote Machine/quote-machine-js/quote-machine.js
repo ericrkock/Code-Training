00056aa775e560242a5b96eec44185dd4475f8fd
Eric R. Kock <eric.r.kock@gmail.com>
2019-02-03 19:18:13 +0000

    Update JS and REACT

---
+++ FreeCodeCamp/Projects/Front End Libraries/Random Quote Machine/quote-machine-js/quote-machine.js
@@ -0,0 +1,60 @@
+/* Random Quote Machine */
+/* A Free Code Camp project challenge
+** 
+** Rules: read the README.md file */
+
+// Load JSON File //
+/*
+function loadJSON(callback) {   
+
+   var xobj = new XMLHttpRequest();
+       xobj.overrideMimeType("application/json");
+   xobj.open('GET', 'quotes.json', true); 
+   xobj.onreadystatechange = function () {
+         if (xobj.readyState == 4 && xobj.status == "200") {
+           // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+           callback(xobj.responseText);
+         }
+   };
+   xobj.send(null);  
+}
+
+init();
+function init() {
+   loadJSON(function(response) {
+    // Parse JSON string into object
+      var actual_JSON = JSON.parse(response);
+      console.log(actual_JSON);
+   });
+  }
+*/
+var currentQuote, newQuote;
+var currentAuthor, newAuthor;
+
+document.querySelector('#tweet-quote').addEventListener('click', tweetQuote);
+document.querySelector('#tumblr-quote').addEventListener('click', tumblrQuote);
+document.querySelector('#new-quote').addEventListener('click', displayQuote);
+
+function tweetQuote() {
+   // Tweet active post on Twitter
+   document.getElementById('text'). textContent = "This tweet function is coming soon";
+}
+
+function tumblrQuote() {
+   // Post active quote on Tumblr
+   document.getElementById('text'). textContent = "This post function is coming soon";
+}
+
+function displayQuote() {
+   // Choose a random quote from json file
+   randomQuoteID = JSON.parse(quotes[Math.floor(Math.random() * quotes.length)]); // Still need to connect with JSON file
+   console.log(randomQuoteId);
+
+   // Display Quote & Author
+   document.getElementById('text'). textContent = quotes[randomQuoteId].quote;
+   document.getElementById('author').textContent = quotes[randomQuoteID].author;
+   // To delete Once connection is made with JSON
+   //document.getElementById('text'). textContent = "Display New Quote";
+   //document.getElementById('author').textContent = "-New Author\'s name";
+}
+
---
+++ FreeCodeCamp/Projects/Front End Libraries/Random Quote Machine/quote-machine-react/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faIgloo } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF, faTumblr, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import './App.css';
+// import * as data from "quotes.json";
+library.add(faIgloo, faTumblr, faTwitter, faFacebookF);
+
+// Coding Starts here //
+// *=*=*=*=*=*=*=*=*= //
+
+class App extends Component {
+  render() {
+    return (
+      <div className="body">
+        <header className="quote-header">
+          <h1>Random Quote Machine</h1>
+        </header>
+        <div className="quote-box">
+          <div className="quote-text" style={{opacity: 1}}>
+            <i className="fa fa-quote-left"></i>
+            <span id="text"> Quote comes here </span>
+            <i className="fa fa-quote-right"></i>
+          </div>
+          <div className="quote-author">
+            <span id="author">
+              - Author's name comes here
+            </span>
+          </div>
+          <div className="quote-buttons">
+            <button id="facebook-quote" className="facebook-button" title="Share this quote on Facebook" target="_blank" href="#"><FontAwesomeIcon icon={faFacebookF} /></button>
+            <button id="tweet-quote" className="tweet-button" title="Tweet this code" target="_blank" href="#"><FontAwesomeIcon icon={faTwitter} /></button>
+            <button id="tumblr-quote" className="tumblr-button" title="Post this quote on Tumblr" target="_blank" href="#"><FontAwesomeIcon icon={faTumblr} /></button>
+            <button id="new-quote" className="next-quote-button" onClick="displayQuote()">New Quote</button>
+          </div>
+
+        </div>
+        <footer>
+          <div className="footer">
+            Presented by <a target="_blank" href="https://successwizard.eu">Eric R. Kock</a>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+function displayQuote() {
+  document.querySelector(#text).textContent = "Display Quote";
+}
+export default App;
