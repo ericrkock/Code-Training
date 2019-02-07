57e8e77467a5501fd7be9e4f98859e5783d72988
Eric R. Kock <eric.r.kock@gmail.com>
2019-02-07 19:46:02 +0000

    Final Updates Pomodoro Clock - DT

--- FreeCodeCamp/Projects/Front End Libraries/Pomodoro Clock/pomodoro-clock.js
+++ FreeCodeCamp/Projects/Front End Libraries/Pomodoro Clock/pomodoro-clock.js
@@ -3,77 +3,113 @@
 // A FreeCodeCamp Project Challenge 
 // Presented by Eric R. Kock - Feb. 2019
 
-/* Variables Declaration */
-var pomodoroSwitch = "session";
-var startStop = "pause";
-var sessionInterval, breakInterval;
+let countdown = 0; // variable to set/clear intervals
+let seconds = 1500; // 1500 seconds left on the clock
+let workTime = 25; // 25
+let breakTime = 5; // 5
+let increment = 1;
+let isBreak = false;
+let isStarted = true;
 
+const status = document.querySelector("#timer-label");
+const timerDisplay = document.querySelector("#time-left");
+const startBtn = document.querySelector("#start_stop");
+const resetBtn = document.querySelector("#reset");
+const workMin = document.querySelector("#session-length");
+const breakMin = document.querySelector("#break-length");
 
-/* HTML Document EventListeners Declaration */
-document.querySelector("#break-decrement").addEventListener("click", breakDecrement);
-document.querySelector("#break-increment").addEventListener("click", breakIncrement);
-document.querySelector("#session-decrement").addEventListener("click", sessionDecrement);
-document.querySelector("#session-increment").addEventListener("click", sessionIncrement);
-document.querySelector("#start_stop").addEventListener("click", switchStatus);
-document.querySelector("#reset").addEventListener("click", reset);
+const alarm = document.createElement('audio'); alarm.setAttribute("src", "https://www.soundjay.com/misc/sounds/bell-ringing-05.mp3");
 
-function breakDecrement() {
-   document.getElementById("break-length").textContent = Number(+document.getElementById("break-length").textContent+1);
+// Add EventListeners
+startBtn.addEventListener('click', () => {
+   clearInterval(countdown);
+   isStarted = !isStarted;
+   if (!isStarted) countdown = setInterval(timer, 1000);
+})
+
+resetBtn.addEventListener('click', () => {
+   clearInterval(countdown);
+   seconds = 1500;
+   workTime = 25;
+   breakTime = 5;
+   countdown = 0;
+   isStarted = true;
+   isBreak = false;
+})
+
+// Functions
+function timer() {
+   seconds --;
+   if (seconds < 0) {
+     clearInterval(countdown);
+     alarm.currentTime = 0;
+     alarm.play();
+     seconds = (isBreak ? breakTime : workTime) * 60;
+     isStarted = true;
+     countdown = 0;
+     if (isBreak == true) {
+        isBreak = false;
+     } else {
+        isBreak = true;
+     }
+     continueTimer();
+   } 
+   updateHTML();
 }
 
-function breakIncrement() {
-   let x = parseInt(+document.getElementById("break-length").textContent-1);
-   if ( x != 0) document.getElementById("break-length").textContent = parseInt(+document.getElementById("break-length").textContent-1);
+function continueTimer() {
+   clearInterval(countdown);
+   isStarted = !isStarted;
+   if (!isStarted) countdown = setInterval(timer, 1000);
 }
 
-function sessionDecrement() {
-   document.getElementById("session-length").textContent = Number(+document.getElementById("session-length").textContent+1);
-   document.getElementById("m").textContent = parseInt(+document.getElementById("m").textContent+1);
+// Declare Actions to Buttons
+document.querySelector("#session-decrement").onclick = function() {workDuration < 60 ? workDuration += increment : workDuration;}
+document.querySelector("#session-increment").onclick = function() {workDuration > 0 ? workDuration -= increment : workDuration;}
+document.querySelector("#break-decrement").onclick = function() {breakDuration < 60 ? breakDuration += increment : breakDuration;}
+document.querySelector("#break-increment").onclick = function() {breakDuration > 0 ? breakDuration -= increment : breakDuration;}
+
+
+let incrementFunctions =
+   {"#session-decrement": function () { workTime = Math.min(workTime + increment, 59); seconds = workTime * 60;},
+      "#session-increment": function () { workTime = Math.max(workTime - increment, 1); seconds = workTime * 60;},
+      "#break-decrement": function () { breakTime = Math.min(breakTime + increment, 59)},
+      "#break-increment": function () { breakTime = Math.max(breakTime - increment, 1)}};
+
+for (var key in incrementFunctions) {if (incrementFunctions.hasOwnProperty(key)) document.querySelector(key).onclick = incrementFunctions[key];}
+
+function countdownDisplay() {
+   let minutes = Math.floor(seconds / 60);
+   let remainderSeconds = seconds % 60;
+   timerDisplay.textContent = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
 }
-
-function sessionIncrement() {
-   let x = parseInt(+document.getElementById("session-length").textContent-1);
-   if ( x != 0) {
-      document.getElementById("session-length").textContent = parseInt(+document.getElementById("session-length").textContent-1);
-      document.getElementById("m").textContent = parseInt(+document.getElementById("m").textContent-1);
-   }
-}
-
-function switchStatus() {
-
-   if ( startStop == "start") {
-      startStop = "pause";
+ 
+function buttonDisplay() {
+   if (isStarted && countdown === 0) {
+     startBtn.textContent = "START CountDown";
+     document.documentElement.style.setProperty("--play", "navy");
+   } else if (isStarted && countdown !== 0) {
+     startBtn.textContent = "Continue CountDown"; 
+     document.documentElement.style.setProperty("--play", "green");
    } else {
-      startStop = "start";
+     startBtn.textContent = "Pause CountDown";
+     document.documentElement.style.setProperty("--play", "red");
    }
-   countDown();
 }
-
-function countDown() {
-   
-   if (startStop == "start") {
-      if (pomodoroSwitch == "session") {
-
-         pomodoroSwitch ="pause";
-         document.documentElement.style.setProperty("--switch", "green");
-      } else {
-
-         pomodoroSwitch ="session";
-         document.documentElement.style.setProperty("--switch", "navy");
-      }
-      console.log("Count Down is running");
+ 
+function updateHTML() {
+   countdownDisplay();
+   buttonDisplay();
+   if (!isBreak) {
+      status.textContent = "Keep Working";
+      document.documentElement.style.setProperty("--theme", "navy");
    } else {
-
-      console.log("Count Down has Stopped!")
+      status.textContent = "Take a Break!";
+      document.documentElement.style.setProperty("--theme", "green");
    }
-   console.log(startStop);
+   workMin.textContent = workTime;
+   breakMin.textContent = breakTime;
 }
 
-function reset() {
-   document.getElementById("break-length").textContent = "5";
-   document.getElementById("session-length").textContent = "25";
-   document.getElementById("m").textContent = "25";
-   document.documentElement.style.setProperty("--switch", "navy");
-   pomodoroSwitch = "session";
-   startStop = "pause";
-}
+updateHTML();
+document.onclick = updateHTML;
