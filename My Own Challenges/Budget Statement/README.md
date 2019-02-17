37dfa8785b87ba578e5a5418839c82bf71d14e19
Eric R. Kock <eric.r.kock@gmail.com>
2019-02-17 19:30:22 +0000

    Setup Budget Statement Project Challenge - DT

---
+++ My Own Challenges/Budget Statement/README.md
@@ -0,0 +1,2 @@
+Based on the Full JS Course of Jonas Schmedtmann at Udemy
+   Section 6 - The Budget App Project
---
+++ My Own Challenges/Budget Statement/budget-statement.js
@@ -0,0 +1,4 @@
+// Budget Statement
+// ================
+
+// Based on the "The Complete JavaScript Course" Section 6 of Jonas Schmedtmann at Udemy
